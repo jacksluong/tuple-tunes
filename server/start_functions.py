f66601369648ea6a22b48a5f8914512e66a0f580
blei37 <blei37168@gmail.com>
2021-04-28 16:05:35 -0400

    adde starter in game file

--- server/start_functions.py
+++ server/start_functions.py
@@ -1,4 +1,3 @@
-import numpy
 import sqlite3
 import datetime
 
@@ -6,19 +5,6 @@ moosic_db = '/var/jail/home/team59/moosic/moosic.db'
 
 #hard coded stuff: all the major minor scales
 #TODO: set up frequencies with each note
-key_scale_notes = {
-    "C Major": ["C", "D", "E", "F", "G", "A", "B", "C"],
-    "G Major": ["G", "A", "B", "C", "D", "E", "F#", "G"],
-    "D Major": ["D", "E", "F#", "G", "A", "B", "C#", "D"],
-    "A Major": ["A", "B", "C#", "D", "E", "F#", "G#", "A"],
-    "E Major": ["E", "F#", "G#", "A", "B", "C#", "D#", "E"],
-    "B Major": ["B", "C#", "D#", "E", "F#", "G#", "A#", "B"],
-    "F# Major": ["F#", "G#", "A#", "B", "C#", "D#", "F", "F"],
-    "Db Major": ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C", "Db"],
-    "Eb Major": ["Eb", "F", "G", "Ab", "Bb", "C", "D", "Eb"],
-    "Bb Major": ["Bb", "C", "D", "Eb", "F", "G", "A", "Bb"],
-    "F Major": ["F", "G", "A", "Bb", "C", "D", "E", "F"],
-}
 
 GAME_ROOM_CAPACITY_LIMIT = 10
 

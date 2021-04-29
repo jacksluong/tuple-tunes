1bfc5ab52e729a5a51c7a1d2b974e2fae1563660
jan_park49 <jan.h.park@gmail.com>
2021-04-29 17:05:24 -0400

    started writing measure

--- tuple_tunes/audio.ino
+++ tuple_tunes/audio.ino
@@ -5,7 +5,7 @@
 uint32_t last_played = 0;
 
 struct Measure {
-  double notes[16]; //the notes (array of doubles containing frequencies in Hz. I used https://pages.mtu.edu/~suits/notefreqs.html
+  int notes[16]; //the notes (array of integers containing indices, later to find in note_freqs.)
   float note_period; //the timing of each note in milliseconds (take bpm, scale appropriately for note. This is 15000/bpm.
 };
 
@@ -18,4 +18,11 @@ void play_note(int index) {
 void stop_sound() {
   ledcWriteTone(AUDIO_PWM, 0);
   Serial.println("Off sound");
+}
+
+void play_measure(Measure measure) {
+  int m_index = 0;
+  while (m_index < 16) {
+    m_index = m_index + 1;
+  }
 }
